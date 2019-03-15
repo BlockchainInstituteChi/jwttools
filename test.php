3a66f9f6bafb3f8d50f8520e6b0f983f82a8e1c1
Alex <alex@theblockchaininstitute.org>
2019-03-15 13:48:36 -0500

    working IPFS mode base 58 decode

--- test.php
+++ test.php
@@ -3,19 +3,13 @@
 	use RuntimeException;
 	use Tuupola\Base58;
 
-	$base58check = new Base58([
-	    "characters" => Base58::BITCOIN,
-	    "check" => true,
+	$base58 = new Base58([
+	    "characters" => Base58::IPFS,
 	    "version" => 0x00
 	]);
 
-	print $base58check->encode("Hello world!"); /* 19wWTEnNTWna86WmtFsTAr5 */
-
-	try {
-	    $base58check->decode("19wWTEnNTWna86WmtFsTArX");
-	} catch (RuntimeException $exception) {
-	    /* Checksum "84fec52c" does not match the expected "84fec512" */
-	    print $exception->getMessage();
-	}
+	
+	print $base58->decode("2ot1hCuVAL6nQ3NQryjkBARGtsj4rsao575");
+	
 
 ?>
