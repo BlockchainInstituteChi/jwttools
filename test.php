1639418b7be9e6128f2e96165834e2305c02c930
Alex <alex@theblockchaininstitute.org>
2019-03-18 16:02:02 -0500

    fixed ascii to hex handling in function call encoding, pending further revisions to custom pad left function to resolve the correct callstring

--- test.php
+++ test.php
@@ -9,47 +9,115 @@ use Tuupola\Base58;
 $encodedMNID = "2ot1hCuVAL6nQ3NQryjkBARGtsj4rsao575";
 $BCIencodedMNID = "2ojEtUXBK2J75eCBazz4tncEWE18oFWrnfJ";
 
+callRegistry ("uPortProfileIPFS1220", $encodedMNID, $encodedMNID, 'placeHolderCallback');
+
+function placeholderCallback ($result) {
+	echo $result;
+}
+
+function callRegistry ($registrationIdentifier, $issuerId, $subjectId, $callback) {
+
+	$issuer = eaeDecode($issuerId);
+	$subject = eaeDecode($subjectId);
+
+	$networks = getNetworks();
+
+	if ( $issuer['network'] !== $subject['network'] ) {
+		call_user_func($callback, "Error: Subject and Issuer must be in the same network!");
+	}
+
+	if (!$networks[$issuer['network']]) {
+      call_user_func($callback, 'Network id ' . $issuer['network'] . ' is not configured');
+    } 
+    
+    $rpcUrl = $networks[$issuer['network']]['registry'];
+    $registryAddress = $networks[$issuer['network']]['registry'];
+
+    $functionSignature = '0x447885f0';
+
+    $callString = encodeFunctionCall($functionSignature, $registrationIdentifier, $issuer['address'], $subject['address']);
+
+
+    echo "\r\n\r\n" . $callString . "\r\n";
 
 
 
-eaeDecode($encodedMNID);
+}
 
+function encodeFunctionCall ($functionSignature, $registrationIdentifier, $issuer, $subject) {
+	echo "\r\nencodeFunctionCall\r\n", $functionSignature, " ", $registrationIdentifier, " ", $issuer,  " ", $subject;
+	$callString = $functionSignature;
 
+	$regStub = String2Hex($registrationIdentifier);
+	$issStub = subStr($issuer, (-1)*(sizeof($issuer) - 3));
+	$subStub = subStr($subject, (-1)*(sizeof($issuer) - 3));
+
+	echo "\r\nStubs: ", $regStub, $issStub, $subStub, "\r\n"; 
+
+	$callString .= pad('0000000000000000000000000000000000000000000000000000000000000000', $regStub, false);
+	$callString .= pad('0000000000000000000000000000000000000000000000000000000000000000', $issStub, true);
+	$callString .= pad('0000000000000000000000000000000000000000000000000000000000000000', $subStub, true);
+	return $callString;
+
+}
+
+function pad ($pad, $str, $padLeft) {
+	echo "\r\n\r\npad triggered\r\n", $pad, " ",  $str,  " ", $padLeft;
+	if ( gettype($str) == "undefined" ) {
+		return $pad;
+	}
+	if ( $padLeft ) {
+		return substr( ($pad . $str), (-1)*sizeof($pad) );
+	} else {
+		return substr( ($str . $pad), 0, (-1)*sizeof($pad) );
+	}
+}
+
+function String2Hex($string){
+    $hex='';
+    for ($i=0; $i < strlen($string); $i++){
+        $hex .= dechex(ord($string[$i]));
+    }
+    return $hex;
+}
+ 
 
 function eaeDecode ($payload) {
 	$base58 = new Base58([
 	    "characters" => Base58::IPFS,
 	    "version" => 0x00
 	]);
-
-	echo "\r\n** eaeDecode ran with encoded payload ** \r\n", $payload, "\r\ntype: \r\n", gettype($payload), "\r\n";
 	$data = unpack( "C*", $base58->decode($payload) );
-	echo "\r\ndecoded data was \r\n", $data, 
-			"\r\ntype: \r\n", gettype($data), 
-			"\r\n length: ", sizeof($data), 
-			"\r\n first: ", $data[1], 
-			"\r\n second: ", $data[2],
-			"\r\n third: ", $data[3],
-			"\r\n fourth: ", $data[4],
-			"\r\n";
 	$netLength = sizeof($data) - 24;
-	echo "\r\nnetlength is \r\n", $netLength, 
-			"\r\ntype: \r\n", gettype($netLength), "\r\n";
 	$network = array_slice($data, 1, $netLength - 1);
-	echo "\r\nnetwork is \r\n", var_dump($network), 
-			"\r\ntype: \r\n", gettype($network), "\r\n";
 	$address = array_slice($data, $netLength, 20 + $netLength - 2);
-	echo "\r\naddress is \r\n", var_dump($address), 
-			"\r\ntype: \r\n", gettype($address), "\r\n";
-
 	$network = "0x" . encodeByteArrayToHex($network);
 	$address = "0x" . encodeByteArrayToHex($address);
+	return [
+		"address" => $address,
+		"network" => $network
+	];				
+}
 
-	echo "\r\nnetwork: ", $network, 
-			"\r\naddress: ", $address,
-			"\r\n";
-
-				
+function getNetworks () {
+	return [
+		  '0x01' => [
+		  	    'registry' => '0xab5c8051b9a1df1aab0149f8b0630848b7ecabf6',
+    			'rpcUrl' => 'https://mainnet.infura.io'
+		  ], 
+		  '0x02' => [
+		  	    'registry' => '0x41566e3a081f5032bdcad470adb797635ddfe1f0',
+    			'rpcUrl' => 'https://ropsten.infura.io'
+		  ], 
+		  '0x03' => [
+		  	    'registry' => '0x5f8e9351dc2d238fb878b6ae43aa740d62fc9758',
+    			'rpcUrl' => 'https://kovan.infura.io'
+		  ],
+		  '0x04' => [
+		  	    'registry' => '0x2cc31912b2b0f3075a87b3640923d45a26cef3ee',
+    			'rpcUrl' => 'https://rinkeby.infura.io'
+		  ]
+	];
 }
 
 function encodeByteArrayToHex ($byteArray) {
@@ -61,32 +129,5 @@ function encodeByteArrayToHex ($byteArray) {
 	return $hex;
 
 }
-	
-function eaeDecodeHex ($payload) {
-	$base58 = new Base58([
-	    "characters" => Base58::IPFS,
-	    "version" => 0x00
-	]);
 
-	echo "\r\n** eaeDecode ran with encoded payload ** \r\n", $payload, "\r\ntype: \r\n", gettype($payload), "\r\n";
-	$data = unpack( "H*", $base58->decode($payload) )[1];
-	echo "\r\ndecoded data was \r\n", $data, 
-			"\r\ntype: \r\n", gettype($data), 
-			"\r\n length: ", strlen($data), 
-			"\r\n first: ", $data[0], 
-			"\r\n second: ", $data[1],
-			"\r\n third: ", $data[2],
-			"\r\n fourth: ", $data[3],
-			"\r\n";
-	$netLength = strlen($data) - 24;
-	echo "\r\nnetlength is \r\n", $netLength, 
-			"\r\ntype: \r\n", gettype($netLength), "\r\n";
-	$network = substr(1, $netLength);
-	echo "\r\nnetwork is \r\n", $network, 
-			"\r\ntype: \r\n", gettype($network), "\r\n";
-	$address = substr($netLength, 20 + $netLength);
-	echo "\r\naddress is \r\n", $address, 
-			"\r\ntype: \r\n", gettype($address), "\r\n";
-}
-	
 ?>
