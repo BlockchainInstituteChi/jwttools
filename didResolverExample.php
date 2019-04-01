6538db757b6930f77aaa531fc565dfeadf0bdaf5
Alex <alex@theblockchaininstitute.org>
2019-04-01 15:37:15 -0500

    adding example details for using jwt to resolve Did rather than raw mnid

--- didResolverExample.php
+++ didResolverExample.php
@@ -6,16 +6,18 @@
 
 	use Blockchaininstitute\jwtTools as jwtTools;
 
-	$encodedMNID = "2ot1hCuVAL6nQ3NQryjkBARGtsj4rsao575";
-	$BCIencodedMNID = "2ojEtUXBK2J75eCBazz4tncEWE18oFWrnfJ";
-
 	$jwtTools = new jwtTools();
 	
+	$jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpYXQiOjE1NTM4MDE4OTYsImV4cCI6MTU1Mzg4ODI5NiwiYXVkIjoiMm9qRXRVWEJLMko3NWVDQmF6ejR0bmNFV0UxOG9GV3JuZkoiLCJ0eXBlIjoic2hhcmVSZXNwIiwibmFkIjoiMm90MWhDdVZBTDZuUTNOUXJ5amtCQVJHdHNqNHJzYW81NzUiLCJvd24iOnsibmFtZSI6IkFsZXgifSwicmVxIjoiZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKRlV6STFOa3NpZlEuZXlKcFlYUWlPakUxTlRNNE1ERTRPVEFzSW5KbGNYVmxjM1JsWkNJNld5SnVZVzFsSWwwc0ltTmhiR3hpWVdOcklqb2lhSFIwY0hNNkx5OWphR0Z6Y1hWcExuVndiM0owTG0xbEwyRndhUzkyTVM5MGIzQnBZeTlvTURoelRVODBOMjVYY1VzMVYyOVRJaXdpYm1WMElqb2lNSGcwSWl3aWRIbHdaU0k2SW5Ob1lYSmxVbVZ4SWl3aWFYTnpJam9pTW05cVJYUlZXRUpMTWtvM05XVkRRbUY2ZWpSMGJtTkZWMFV4T0c5R1YzSnVaa29pZlEuaURoNWZ4UjZDdEpHV0VBcjg1VzBpd0JXMmhxOTl5UnE2T0ZQbUxpVGxlRmNoclItd3VYcWlGTmI1R203SUQ4VGxsR2RMRGpzSlU4NkV3U0E2dFU2b3ciLCJpc3MiOiIyb3QxaEN1VkFMNm5RM05Rcnlqa0JBUkd0c2o0cnNhbzU3NSJ9.vFslRV7OGpfCAwQ9HDqr1BoBYNXlzyHjZiJrT4_0exgbrVXTYjbvJ3_6GGtI2yKATxjOUuX5EToNBcTXyPLBUg";
+
+	$encodedMNID = $jwtTools->getSenderMnid($jwt); 
+	$BCIencodedMNID = $jwtTools->getAudienceMnid($jwt);
+
 	function placeholderCallback ($result) {
 		echo $result;
 	}
 
-	$jwtTools->resolve_did("uPortProfileIPFS1220", $encodedMNID, 'placeHolderCallback');
+	$jwtTools->resolve_did("uPortProfileIPFS1220", $jwt, 'placeHolderCallback');
 
 	// If you're trying to retrieve uport DIDs, the following function may help:
 	function callInfura ($result) {
