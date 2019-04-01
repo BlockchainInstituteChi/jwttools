b0559e750d6c5bf6931a0e1e1d1be6dd37f36ec3
Alex <alex@theblockchaininstitute.org>
2019-04-01 15:08:23 -0500

    working validation example

--- didResolverExample.php
+++ didResolverExample.php
@@ -9,13 +9,13 @@
 	$encodedMNID = "2ot1hCuVAL6nQ3NQryjkBARGtsj4rsao575";
 	$BCIencodedMNID = "2ojEtUXBK2J75eCBazz4tncEWE18oFWrnfJ";
 
-	$didResolver = new jwtTools();
+	$jwtTools = new jwtTools();
 	
 	function placeholderCallback ($result) {
 		echo $result;
 	}
 
-	$didResolver->resolve_did("uPortProfileIPFS1220", $encodedMNID, 'placeHolderCallback');
+	$jwtTools->resolve_did("uPortProfileIPFS1220", $encodedMNID, 'placeHolderCallback');
 
 	// If you're trying to retrieve uport DIDs, the following function may help:
 	function callInfura ($result) {
